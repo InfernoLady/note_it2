56c499eb21d326e3d969141ea8f1785251404c20
marina <maky.rtf@gmail.com>
2015-05-19 12:03:43 +0200

    Implement comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,19 @@
 class CommentsController < ApplicationController
-
+before_action :authenticate_user!
 	def create
 		@note = Note.find(params[:note_id])
-		@comment = @note.comments.create(params[:comment].permit(:content))
-		
-		redirect_to note_path(@note)
+		@comment = @note.comments.build(params[:comment].permit(:content))
+		@comment.user_id = current_user.id 
+		if @comment.save
+			redirect_to note_path(@note)
+		end
 	end
 
+	def destroy
+		@note = Note.find(params[:note_id])
+		@comment = @note.comments.find(params[:id])
+		@comment.destroy
+
+		redirect_to note_path(@note)
+	end
 end
